4e6d92f608b0c6c7672e80696dd3eaf16375f5a3
k-jank <157026427+k-jank@users.noreply.github.com>
2024-07-28 15:47:04 +0700

    Delete myproject directory

--- myproject/products/urls.py
+++
@@ -1,7 +0,0 @@
-# products/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.product_list, name='product_list'),
-]
--- myproject/products/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-# Create your views here.
-
-
-def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
